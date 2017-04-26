2350344927cdd23884fa4230a1c73761072583dd
Johnny Watson <johnny@streetchange.org.uk>
2017-04-26 17:26:21 +0100

    Add find artist by id function

--- models/artist.rb
+++ models/artist.rb
@@ -5,48 +5,54 @@ attr_reader :id
 attr_accessor :name, :genre
 
 
-def initialize(params)
-  @id = params['id'].to_i if params['id']
-  @name = params['name']
-  @genre = params['genre']
-end 
+  def initialize(params)
+    @id = params['id'].to_i if params['id']
+    @name = params['name']
+    @genre = params['genre']
+  end 
 
-def save()
-  sql = "
-  INSERT INTO artists (
-  name,
-  genre
-  ) VALUES (
-  '#{@name}',
-  '#{@genre}'
-  )
-  RETURNING id;"
+  def save()
+    sql = "
+    INSERT INTO artists (
+    name,
+    genre
+    ) VALUES (
+    '#{@name}',
+    '#{@genre}'
+    )
+    RETURNING id;"
 
-  result = SqlRunner.run(sql)
-  @id = result[0]["id"].to_i
-end 
+    result = SqlRunner.run(sql)
+    @id = result[0]["id"].to_i
+  end 
 
-def update()
-  sql = "
-  UPDATE artists SET(
-  name,
-  genre
-  ) = (
-  '#{@name}',
-  '#{@genre}'
-  )
-  WHERE id = #{@id};"
-  SqlRunner.run(sql)
-end
+  def update()
+    sql = "
+    UPDATE artists SET(
+    name,
+    genre
+    ) = (
+    '#{@name}',
+    '#{@genre}'
+    )
+    WHERE id = #{@id};"
+    SqlRunner.run(sql)
+  end
 
 
-def Artist.all()
-  sql = "SELECT * FROM artists;"
-  return SqlRunner.run(sql).map { |artist| Artist.new(artist) }
-end
+  def Artist.all()
+    sql = "SELECT * FROM artists;"
+    return SqlRunner.run(sql).map { |artist| Artist.new(artist) }
+  end
 
-def delete()
-  sql = "DELETE FROM artists WHERE id = #{@id};"
-  SqlRunner.run(sql)
-end 
+  def delete()
+    sql = "DELETE FROM artists WHERE id = #{@id};"
+    SqlRunner.run(sql)
+  end 
+
+  def Artist.find(id)
+    sql = "SELECT * FROM artists WHERE id = #{id}"
+      artist = Artist.new(SqlRunner.run(sql)[0])
+      return artist
+  end 
 end 
