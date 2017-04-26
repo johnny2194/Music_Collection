d168f4c9a0a5ee8aead67e423a97743f73fe0642
Johnny Watson <johnny@streetchange.org.uk>
2017-04-26 18:15:17 +0100

    Add update album funciton

--- console.rb
+++ console.rb
@@ -1,11 +1,24 @@
 require('pry-byebug')
 require_relative('models/artist.rb')
+require_relative('models/album.rb')
 
 artist1 = Artist.new({
  'name' => 'Michael Jackson',
  'genre' => 'Pop'
   })
 artist1.save()
+
+album1 = Album.new({
+ 'name' => 'Thriller',
+ 'year' => '1982'
+  })
+album1.save()
+
+album2 = Album.new({
+ 'name' => 'Bad',
+ 'year' => '1987'
+  })
+album2.save()
 
 binding.pry 
 nil 
--- models/album.rb
+++ models/album.rb
@@ -6,10 +6,37 @@ class Album
   attr_reader :id
 
   def initialize(params)
+    @id = params['id'].to_i if params['id']
     @name = params['name']
     @year = params['year'].to_i
-    @id = params['id'].to_i if params['id']
     @artist_id = params['artist_id'].to_i if params['artist_id']
+  end
+
+  def save()
+    sql = "
+    INSERT INTO albums (
+    name,
+    year
+    ) VALUES (
+    '#{@name}',
+    #{@year}
+    )
+    RETURNING id;"
+    result = SqlRunner.run(sql)
+    @id = result[0]["id"].to_i
+  end 
+
+  def update()
+    sql = "
+    UPDATE albums SET(
+    name,
+    year
+    ) = (
+    '#{@name}',
+    #{@year}
+    )
+    WHERE id = #{@id};"
+    SqlRunner.run(sql)
   end
 
 end 
