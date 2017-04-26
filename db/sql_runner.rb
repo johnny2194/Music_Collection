7e1273a45661a28eef1292e87135ef4a6faf2245
Johnny Watson <johnny@streetchange.org.uk>
2017-04-26 16:06:43 +0100

    create sql_runner.rb and add SqlRunner class with SqlRunner.run(sql) function

---
+++ db/sql_runner.rb
@@ -0,0 +1,13 @@
+require ('pg')
+
+  class SqlRunner 
+    def SqlRunner.run(sql)
+      begin
+        db = PG.connect({dbname: 'music_collection', host: 'localhost'})
+        result = db.exec(sql)
+      ensure
+        db.close()
+      end 
+      return result
+    end 
+  end 
